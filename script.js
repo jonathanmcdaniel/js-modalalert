37ef0e97a4439f4919f5d1d37f95e8e5f24b81ef
Jonathan McDaniel <jonmd24@gmail.com>
2013-04-25 14:59:26 -0400

    Basic Functionality
    Created the alert in CSS and added functionality in JavaScript

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-function loadAlert(){
-    
+function showAlert(){
+    var alertTitle = document.getElementById('alerttitle');
+    var alertBody = document.getElementById('alertbody');
+    alertTitle.innerHTML = '';
+    alertBody.innerHTML = '';
+    document.getElementById('acontainer').style.visibility = 'visible';
+}
+function hideAlert(){
+    document.getElementById('acontainer').style.visibility = 'hidden';
 }

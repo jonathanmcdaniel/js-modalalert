fd0ecf500846b23f3fab5f913951fb53b077c8ce
Jonathan McDaniel <jonmd24@gmail.com>
2013-04-25 15:18:12 -0400

    Added custom text sections
    Added customizability from the parameters

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-function showAlert(){
+function showAlert(title, body, button){
     var alertTitle = document.getElementById('alerttitle');
     var alertBody = document.getElementById('alertbody');
-    alertTitle.innerHTML = '';
-    alertBody.innerHTML = '';
+    var alertButton = document.getElementById('closebutton');
+    alertTitle.innerHTML = title;
+    alertBody.innerHTML = body;
+    alertButton.innerHTML = button;
     document.getElementById('acontainer').style.visibility = 'visible';
 }
 function hideAlert(){

5599d135a7b48c00316699bb246fa68468ca1569
Jonathan McDaniel <jonmd24@gmail.com>
2013-04-26 13:11:28 -0400

    Added field customizabily + style changes

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
-function showAlert(title, body, button, form, numberOfFields){
+function showAlert(title, body, button, form){
     var alertTitle = document.getElementById('alerttitle');
     var alertBody = document.getElementById('alertbody');
     var alertButton = document.getElementById('closebutton');
+    
+    var buttonValue = new Array();
+        buttonValue[0] = 'First Name';
+        buttonValue[1] = 'Last Name';
+        buttonValue[2] = '###-###-####'
+    
     alertTitle.innerHTML = title;
     if(form == 0){
         alertBody.innerHTML = body;
     }else{
         var formString = '';
-        for(var i = 0; i < numberOfFields; i++){
-            formString = formString + '<input type="text" size="30" id="alertForm' +i +'"><br />';
+        for(var i = 0; i < buttonValue.length; i++){
+            formString = formString + '<input type="text" size="30" id="alertForm' +i +'" placeholder="' +buttonValue[i] +'"><br />';
         }
         alertBody.innerHTML = formString + body;
     }
     alertButton.innerHTML = button;
+    
     document.getElementById('acontainer').style.visibility = 'visible';
 }
 function closeAlert(){

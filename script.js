d9f9e48fb5ce93707645b1fae007dc780cffbd7b
Jonathan McDaniel <jonmd24@gmail.com>
2013-04-25 22:57:18 -0400

    Added form elements
    Form elements will be added from the method call. Limited to text
    fields of predefined width.

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-function showAlert(title, body, button){
+function showAlert(title, body, button, form, numberOfFields){
     var alertTitle = document.getElementById('alerttitle');
     var alertBody = document.getElementById('alertbody');
     var alertButton = document.getElementById('closebutton');
     alertTitle.innerHTML = title;
-    alertBody.innerHTML = body;
+    if(form == 0){
+        alertBody.innerHTML = body;
+    }else{
+        var formString = '';
+        for(var i = 0; i < numberOfFields; i++){
+            formString = formString + '<input type="text" size="30" id="alertForm' +i +'"><br />';
+        }
+        alertBody.innerHTML = formString + body;
+    }
     alertButton.innerHTML = button;
     document.getElementById('acontainer').style.visibility = 'visible';
 }
-function hideAlert(){
+function closeAlert(){
     document.getElementById('acontainer').style.visibility = 'hidden';
 }

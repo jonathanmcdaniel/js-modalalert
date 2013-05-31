93f207b1052f37161f1517c9e2bb5cb117ac52f4
Jonathan McDaniel <jonmd24@gmail.com>
2013-05-31 10:46:10 -0400

    RGB variable order fix

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var buttonValue = new Array();
 var alertInfo = new Array();
-function showAlert(title, body, button, form, red, blue, green, alpha){
+function showAlert(title, body, button, form, red, green, blue, alpha){
     var alertTitle = document.getElementById('alerttitle');
     var alertBody = document.getElementById('alertbody');
     var alertButton = document.getElementById('closebutton');

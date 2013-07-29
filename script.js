2170765421e497f2531b0154f217b414c4162feb
Jonathan McDaniel <jonmd24@gmail.com>
2013-07-29 17:48:24 -0400

    File Commenting

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
+/*
+ * Created by Jonathan McDaniel July 2013
+ * Last Edited: July 29, 2013
+ * http://github.com/jonathanmcdaniel
+ */
+
 var buttonValue = new Array();
 var alertInfo = new Array();
-function showAlert(title, body, button, form, red, green, blue, alpha){
+
+//This function creates the alertview and shows it on screen
+//The parameters to the function determine the number of elements in the view
+function showAlert(title, body, button, form){
+    
     var alertTitle = document.getElementById('alerttitle');
     var alertBody = document.getElementById('alertbody');
     var alertButton = document.getElementById('closebutton');
     
+    //This sets the placeholder text for each element
         buttonValue[0] = 'First Name';
         buttonValue[1] = 'Last Name';
-        buttonValue[2] = 'email@email.com'
+        buttonValue[2] = 'user@domain.com'
     
     alertTitle.innerHTML = title;
+    
+    //This bit determines the purpose for the alertview
+    //If the developer wants the alert to be strictly presentation 0 is set for the form parameter and only the body and title are shown
+    //If the developer wants form elements, the form parameter recieves a 1
+    //The form elements are then created to match the length of the placeholder array and are presented in that order
     if(form == 0){
         alertBody.innerHTML = body;
     }else{
@@ -21,11 +37,19 @@ function showAlert(title, body, button, form, red, green, blue, alpha){
     }
     alertButton.innerHTML = button;
     
+    //Finally, the alertview is presented to the user
     document.getElementById('acontainer').style.visibility = 'visible';
 }
+
+//This function hides the alertview
+//This would be an ideal location to call the getAlertContents() method
 function closeAlert(){
     document.getElementById('acontainer').style.visibility = 'hidden';
 }
+
+
+//This function retrieves the data stored in the alert elements
+//and stores them in the alertInfo array
 function getAlertContents(){
     for(j = 0; j < buttonValue.length; j++){
         var el = document.getElementById('alertForm'+j).value;
